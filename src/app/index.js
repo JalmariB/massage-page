b0377c1466fbbe3e02ecc686033b3a37dfef40b2
Janne B <Janne B>
2017-03-27 17:28:48 +0300

    core

--- src/app/index.js
+++ src/app/index.js
@@ -1,126 +1,46 @@
 
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//create component
-var CustomerForm = React.createClass({
+import {Router, Route, browserHistory, Link} from 'react-router';
 
-getInitialState: function (){
-  return {
-    customerinfo: ['Nimi: ', 'Puhelinnumero: ', 'Sähköposti: ', 'Ajankohta: '],
-    booking: ['Ajanvaraus']
 
-  }
-},
+//Module requires
+var NavComponent = require('./nav')
+var FooterComponent = require('./footer')
+var FirstBlockComponent = require('./firstBlock')
+var SecondBlockComponent = require('./secondBlock')
+var CustomerForm = require('./customerForm');
+var AppointmentTimeComponent = require('./appointmentTime');
+var AppointmentHeader = require('./appointmentHeader')
+var AppointmentSelectDayComponent = require('./appointmentSelectDay')
 
+var App = React.createClass ({
 
   render: function (){
-
     return(
-
-      <div>
-            <form className="customer-info">
-              <h2 className="booking-title">{this.state.booking[0]}</h2>
-                <div className="input-container">
-                  <label htmlFor="name">{this.state.customerinfo[0]}</label>
-                  <input type="text" id="name"></input>
-                </div>
-                <div className="input-container">
-                  <label htmlFor="phonenumber">{this.state.customerinfo[1]}</label>
-                  <input type="tel" id="phonenumber"></input>
-                </div>
-                <div className="input-container">
-                  <label htmlFor="email">{this.state.customerinfo[2]}  </label>
-                  <input type="email" id="email"></input>
-                </div>
-            </form>
-            <button className="primary-button">Jatka</button>
-      </div>
-
-
-    );
+        <Router history={browserHistory}>
+          <Route path={'/app'} component={MainComponent}></Route>
+          <Route path={'/app/appointmentTime'} component={AppointmentTimeComponent}></Route>
+          <Route path={'/app/appointmentSelectDay'} component={AppointmentSelectDayComponent}></Route>
+        </Router>
+      );
   }
 });
 
-
-
-ReactDOM.render(<CustomerForm  />, document.getElementById('form-container'));
-
-
-
-
-
-
-
-
-
-//**********header Component************
-function toggleClassNav (){
-  console.log('The link was clicked.');
-  var menu = document.querySelector('#header-container') // Using a class instead, see note below.
-menu.classList.toggle('hamburger-in');
-}
-var NavComponent = React.createClass({
-
-
-
-
-  // $('.hamburger-menu').click(function() {
-  //      $('#header-container').toggleClass('hamburger-in')
-  // });
-getInitialState: function (){
-  return {
-    navNames: ['Ajanvaraus', 'Hinnasto', 'Janne Berg', 'Yhteystiedot'],
-
-
-  }
-},
-
+// Start
+var MainComponent = React.createClass ({
 
   render: function (){
-
-    var navNames = this.state.navNames;
-    navNames = navNames.map(function(item, index){
-          return (
-            <NavTitlesComponent item={item} key={index}/>
-          )
-    });
-
     return(
-
-      <div>
-        <div onClick={toggleClassNav} className=" hamburger-icon "><i className="fa fa-bars"></i></div>
-        <div>{navNames}</div>
-
-      </div>
-
-
-    );
+        <div>
+          <NavComponent/>
+          <FirstBlockComponent/>
+          <SecondBlockComponent/>
+          <FooterComponent/>
+        </div>
+      );
   }
 });
 
-//create NestedComponent
-var NavTitlesComponent = React.createClass({
-
-
-
-
-  render: function(){
-
-
-    return (
-
-        // <li>
-        //   <div className="nav-item">
-        //       <span className="item-name">{this.props.item}</span>
-        //   </div>
-        // </li>
-        <li className="nav-item">
-              <a href="" className="item-name">{this.props.item}</a>
-        </li>
-    );
-  }
-})
-
-ReactDOM.render(<NavComponent  />, document.getElementById('header-container'));
+ReactDOM.render(<App />, document.getElementById('app-container'));
